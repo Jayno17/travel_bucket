174a7349d826bae74ede1bc9d2be898a99c22fea
Jayno17 <julia.nowak@gmail.com>
2020-02-09 11:01:56 +0000

    added CRUD functions to models

--- models/cities.rb
+++ models/cities.rb
@@ -17,11 +17,37 @@ attr_reader(:name, :id, :country_id)
     @id = results.first()['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE cities SET (name, country_id)
+    = ($1, $2) WHERE id = $3"
+    values = [@name, @country_id]
+    SqlRunner.run(sql, values)
+  end
+
+ def delete()
+   sql = "DELETE FROM cities WHERE id = $1"
+   values = [@id]
+   SqlRunner.run(sql, values)
+ end
+
+ def country()
+   sql = "SELECT * FROM countries WHERE id = $1"
+   values = [@country_id]
+   results = SqlRunner.run(sql, values)
+   return Country.new(results.first)
+ end
 
   def self.all()
     sql = "SELECT * FROM cities"
     results = SqlRunner.run(sql)
     return results.map { |city| City.new(city) }
+  end
+
+  def self.find()
+    sql = "SELECT * FROM cities WHERE id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+    return City.new(results.first)
   end
 
   def self.delete_all()
--- models/countries.rb
+++ models/countries.rb
@@ -17,10 +17,30 @@ attr_reader :name, :id, :continent_id
     @id = results.first()['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE countries SET (name, continent_id)
+    = ($1, $2) WHERE id = $3"
+    values = [@name, @continent_id]
+    SqlRunner.run(sql, values)
+  end
+
+ def delete()
+   sql = "DELETE FROM countries WHERE id = $1"
+   values = [@id]
+   SqlRunner.run(sql, values)
+ end
+
 def self.all()
   sql = "SELECT * FROM countries"
   results = SqlRunner.run(sql)
   return results.map{ |country| Country.new(country)}
+end
+
+def self.find()
+  sql = "SELECT * FROM countries WHERE id = $1"
+  values = [@id]
+  results = SqlRunner.run(sql, values)
+  return Country.new(results.first)
 end
 
   def self.delete_all()
